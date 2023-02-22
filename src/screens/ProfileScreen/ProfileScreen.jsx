bbbff868ffa39cbc236c1d655b1baed395c5450a
Ilya Galtsev <ilyandreev771@gmail.com>
2023-02-22 23:57:45 +0300

    Получение картинки с экрана

--- src/screens/ProfileScreen/ProfileScreen.jsx
+++ src/screens/ProfileScreen/ProfileScreen.jsx
@@ -7,7 +7,6 @@ import { useContext } from "react"
 import { Door } from "akar-icons"
 import "./ProfileScreen.scss"
 
-
 const ProfileScreen = () => {
   const { auth } = useContext(Context)
   const [user] = useAuthState(auth)
@@ -18,14 +17,51 @@ const ProfileScreen = () => {
     })
   }
 
+  let video = document.getElementById("video")
+  let canvas = document.getElementById("canvas")
+  // let contex = canvas.getContext("2d")
+  let photo = document.getElementById("photo")
+  let vendorUrl = window.URL || window.webkitURL
+
+  const getImageWeb = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream
+
+        video.play()
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err)
+      })
+  }
+
+  function setImage() {
+    var context = canvas.getContext("2d")
+    context.drawImage(video, 0, 0, 400, 300)
+
+    var data = canvas.toDataURL("image/png")
+    photo.src = data
+    console.log(data)
+    // console.log("ccilka", URL.createObjectURL(data))
+  }
+
   if (!user) {
-    return <Navigate to="/" replace />
+    return (
+      <Navigate
+        to="/"
+        replace
+      />
+    )
   } else {
     return (
       <ProfileLayout>
         <div className="profile">
           <div className="profile-card">
-            <img src={user.photoURL} alt="ava" />
+            <img
+              src={user.photoURL}
+              alt="ava"
+            />
             <Door
               className="avatar-logout"
               onClick={logOut}
@@ -35,6 +71,36 @@ const ProfileScreen = () => {
             <p>{user.displayName}</p>
             <p>{user.email}</p>
           </div>
+          <video
+            id="video"
+            width="400"
+            height="300"
+            autoplay
+          ></video>
+
+          <canvas
+            id="canvas"
+            width="400"
+            height="300"
+          ></canvas>
+
+          <a
+            onClick={getImageWeb}
+            class="booth-capture-button"
+          >
+            getImageWeb
+          </a>
+          <a
+            onClick={setImage}
+            class="booth-capture-button"
+          >
+            setImage
+          </a>
+          <img
+            id="photo"
+            style={{ background: "rgba(200, 0 , 0, 0.2)" }}
+            alt="Ваша фотография"
+          />
         </div>
       </ProfileLayout>
     )
